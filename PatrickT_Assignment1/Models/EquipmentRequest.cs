440321529db0a8a652a08e96ef58b5c183802941
patrick-ta <patrick.ta451@gmail.com>
2024-09-28 15:10:00 -0400

    Added additional validation for email, phone numbers, and duration

--- PatrickT_Assignment1/Models/EquipmentRequest.cs
+++ PatrickT_Assignment1/Models/EquipmentRequest.cs
@@ -21,14 +21,17 @@ namespace PatrickT_Assignment1.Models
         [Required(ErrorMessage = "Please enter your name")]
         public string? Name { get; set; }
         [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a vaild email address")]
         public string? Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone number must be in the format xxx-xxx-xxxx.")]
         public string? PhoneNumber { get; set; }
         public UserRole Role { get; set; }
         public Equipment EquipmentType { get; set; }
         [Required(ErrorMessage = "Please enter request details")]
         public string? RequestDetails { get; set; }
         [Required(ErrorMessage = "Please enter the duration")]
+        [Range(0, int.MaxValue, ErrorMessage = "Duration must be a positive number")]
         public int? Duration { get; set; }
 
     }
